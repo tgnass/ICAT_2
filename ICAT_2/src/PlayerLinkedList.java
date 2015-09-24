a3fa3116e9db7ecc32cc3a8a7bd4999618a88f8d
mrhorner <mhorner@oakland.edu>
2015-09-24 11:00:23 -0400

    Moved brackets

--- ICAT_2/src/PlayerLinkedList.java
+++ ICAT_2/src/PlayerLinkedList.java
@@ -1,14 +1,17 @@
 public class PlayerLinkedList extends ShellLinkedList {
 
-    public PlayerLinkedList(PlayerNode head, int numberOfItems) {
+    public PlayerLinkedList(PlayerNode head, int numberOfItems) 
+    {
         super(head, numberOfItems);
     }
     
-    public void insert(Player p) {
+    public void insert(Player p) 
+    {
         
     }
     
-    public Player delete(int searchId) {
+    public Player delete(int searchId) 
+    {
         
         return super.head.getPlayer();
     }
--- ICAT_2/src/PlayerNode.java
+++ ICAT_2/src/PlayerNode.java
@@ -8,29 +8,35 @@
  *
  * @author Malik
  */
-public class PlayerNode {
+public class PlayerNode 
+{
     private Player player;
     private PlayerNode next;
     
 
-    public PlayerNode(Player player, PlayerNode next) {
+    public PlayerNode(Player player, PlayerNode next) 
+    {
         this.player = player;
         this.next = next;
     }
 
-    public Player getPlayer() {
+    public Player getPlayer() 
+    {
         return player;
     }
 
-    public PlayerNode getNext() {
+    public PlayerNode getNext() 
+    {
         return next;
     }
 
-    public void setPlayer(Player player) {
+    public void setPlayer(Player player) 
+    {
         this.player = player;
     }
 
-    public void setNext(PlayerNode next) {
+    public void setNext(PlayerNode next) 
+    {
         this.next = next;
     }
     
