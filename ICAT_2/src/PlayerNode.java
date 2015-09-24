7b77d3520f2e879597e50c212fbac8ca32afed16
mrhorner <mhorner@oakland.edu>
2015-09-24 10:19:21 -0400

    Setters, getters, and constructor

---
+++ ICAT_2/src/PlayerNode.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Malik
+ */
+public class PlayerNode {
+    private Player player;
+    private PlayerNode next;
+    
+
+    public PlayerNode(Player player, PlayerNode next) {
+        this.player = player;
+        this.next = next;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public PlayerNode getNext() {
+        return next;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setNext(PlayerNode next) {
+        this.next = next;
+    }
+    
+    
+    
+}
