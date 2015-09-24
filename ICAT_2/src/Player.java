a0caf1232fb41d7aa09f06ff2b49133e2d388aa9
tgnass <tgnass@gmail.com>
2015-09-24 10:14:40 -0400

    Set up Getters and Setters

--- ICAT_2/src/Player.java
+++ ICAT_2/src/Player.java
@@ -1,3 +1,37 @@
 public class Player {
+    private int id;
+    private String name;
+    private String game;
+
+    public Player(int id, String name, String game) {
+        this.id = id;
+        this.name = name;
+        this.game = game;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+    
     
 }
